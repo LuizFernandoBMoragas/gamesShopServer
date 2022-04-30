41022785b9a4d4c113bce0d7007a61059bf4f62d
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-05-01 00:05:10 +0100

    added .env

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+// const cors = require("cors");
+// require("dotenv").config();
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.listen(8080, `Running at port 8080`);
+// const PORT = process.env.PORT || 8080;
+const PORT = 8080;
+
+app.listen(PORT, `Running at ${PORT}`);
