6f3f8106168b1f2dabd4b8559e3d6a86336ec376
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-05-01 23:31:24 +0100

    all end points has been created

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
 const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-// const cors = require("cors");
-// require("dotenv").config();
+app.use(express.json());
 
-// app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+// const dataBase = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "P4d48iv9!",
+//   database: "crudgames",
+// });
 
-app.listen(PORT, `Running at ${PORT}`);
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( ?, ?, ? )";
+
+  dataBase.query(SQL, [name, cost, category], (error, result) => {
+    console.log(error);
+  });
+});
+
+app.get("/getCards", (req, res) => {
+  let SQL = "SELECT * FROM games";
+
+  dataBase.query(SQL, (error, result) => {
+    if (error) console.log(error);
+    else res.send(result);
+  });
+});
+
+app.put("/edit", (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let SQL =
+    "UPDATE games SET name = ?, cost = ?, category = ? WHERE idgames = ?";
+
+  dataBase.query(SQL, [name, cost, category, id], (error, result) => {
+    if (error) console.log(error);
+    else res.send(result);
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  let SQL = "DELETE FROM games WHERE idgames = ?";
+  dataBase.query(SQL, [id], (error, result) => {
+    if (error) console.log(error);
+    else res.send(result);
+  });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Running with express at ${port} port!`);
+});
